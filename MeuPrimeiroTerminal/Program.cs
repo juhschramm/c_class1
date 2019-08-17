aa2cff9428878ce7b56ea70ce30b3712e4061d4e
unknown <mayconfontana@gmail.com>
2019-08-17 14:15:37 -0300

    subindo codigo

--- MeuPrimeiroTerminal/Program.cs
+++ MeuPrimeiroTerminal/Program.cs
@@ -5,12 +5,33 @@ namespace MeuPrimeiroTerminal
     {
         static void Main(string[] args)
         {
-            string txt1 = "Hello World";
-            string txt2 = "Igor";   
-            int idade = 27; 
-            string txt3 = $"{txt1} {txt2} {idade}";
-            
-            Console.WriteLine(txt3);
+            string txt = "Digite a sua idade:";
+            int idade = 0; 
+            Console.WriteLine(txt);     
+            string idadeEmString = Console.ReadLine();
+            idade = Convert.ToInt32(idadeEmString);
+
+            if(idade >= 30){
+
+                Console.WriteLine($"Vc ta velho! hahahaha");    
+
+            }     
+ 
+            else if(idade > 10 && idade < 15){
+
+                Console.WriteLine($"Adolescente! hahahaha");    
+
+            }
+
+
+            else if(idade <= 10){
+
+                Console.WriteLine($"Vc ta novo! hahahaha");    
+
+            }     
+
+            Console.WriteLine($"Vc digitou: {idadeEmString}");
+                        
         }
 
     }
