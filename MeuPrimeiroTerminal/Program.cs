dc0765a67186fe6026e8fbccff0f06f0a1d5dbd9
unknown <mayconfontana@gmail.com>
2019-08-17 16:23:39 -0300

    loop

--- MeuPrimeiroTerminal/Program.cs
+++ MeuPrimeiroTerminal/Program.cs
@@ -5,35 +5,64 @@ namespace MeuPrimeiroTerminal
     {
         static void Main(string[] args)
         {
-            string txt = "Digite a sua idade:";
-            int idade = 0; 
-            Console.WriteLine(txt);     
-            string idadeEmString = Console.ReadLine();
-            idade = Convert.ToInt32(idadeEmString);
+            // string txt = "Digite a sua idade:";
+            // int idade = 0; 
+            // Console.WriteLine(txt);     
+            // string idadeEmString = Console.ReadLine();
+            // idade = Convert.ToInt32(idadeEmString);
+            /*
+            int[] meuArray = new int[5];
 
-            if(idade >= 30){
-
-                Console.WriteLine($"Vc ta velho! hahahaha");    
-
-            }     
- 
-            else if(idade > 10 && idade < 15){
-
-                Console.WriteLine($"Adolescente! hahahaha");    
-
+            for(int i = 0; i <= 4; i++)
+            {
+                
+                meuArray[i] = 10 + i;
+                
             }
 
+            for(int i =0; i < meuArray.Length; i++)
+            {
 
-            else if(idade <= 10){
+                Console.WriteLine(meuArray[i]);
 
-                Console.WriteLine($"Vc ta novo! hahahaha");    
+            }
+            
+            foreach(int variavelIntDoArray in meuArray)
+            {
+                Console.WriteLine(variavelIntDoArray); 
 
-            }     
+            }
+            */
 
-            Console.WriteLine($"Vc digitou: {idadeEmString}");
-                        
+            // string txt = "Digite um numero:";
+            // int numero = 0; 
+            // Console.WriteLine(txt);     
+            // string numeroEmString = Console.ReadLine();
+            // numero = Convert.ToInt32(numeroEmString);
+
+            // for(int i = 0; i <= numero; i++)
+            // {
+                
+            //     Console.WriteLine(i);
+                
+            // }
+
+            Console.WriteLine("Quantos numeros voce quer?");
+            string numerosEmString = Console.ReadLine();
+            int numeros = Convert.ToInt32(numerosEmString);
+
+                int resultado = 1;
+                for(int i =0; i < numeros; i++)
+                {
+                    Console.WriteLine("Escreva seu numero: ");
+                    string numeroEmString = Console.ReadLine();
+                    int numero = Convert.ToInt32(numeroEmString);
+                    resultado *= numero;
+                }
+                Console.WriteLine($"O resultado eh: {resultado}");
+
         }
 
     }
-    
+   
 }
