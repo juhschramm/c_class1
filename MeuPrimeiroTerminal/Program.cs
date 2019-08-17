5292e6ca08230683a62656f8943ccfc6e59dd260
unknown <mayconfontana@gmail.com>
2019-08-17 11:39:08 -0300

    manipulando strings

---
+++ MeuPrimeiroTerminal/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MeuPrimeiroTerminal
+{       class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt1 = "Hello World";
+            string txt2 = "Igor";   
+            int idade = 27; 
+            string txt3 = $"{txt1} {txt2} {idade}";
+            
+            Console.WriteLine(txt3);
+        }
+
+    }
+    
+}
