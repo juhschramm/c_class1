dbac5fc3b49b8c33dda91d8911c7780dd71e2fc8
unknown <mayconfontana@gmail.com>
2019-08-17 17:44:33 -0300

    loop_2

--- MeuPrimeiroTerminal/Program.cs
+++ MeuPrimeiroTerminal/Program.cs
@@ -5,64 +5,34 @@ namespace MeuPrimeiroTerminal
     {
         static void Main(string[] args)
         {
-            // string txt = "Digite a sua idade:";
-            // int idade = 0; 
-            // Console.WriteLine(txt);     
-            // string idadeEmString = Console.ReadLine();
-            // idade = Convert.ToInt32(idadeEmString);
-            /*
-            int[] meuArray = new int[5];
-
-            for(int i = 0; i <= 4; i++)
-            {
-                
-                meuArray[i] = 10 + i;
-                
-            }
-
-            for(int i =0; i < meuArray.Length; i++)
-            {
-
-                Console.WriteLine(meuArray[i]);
-
-            }
+            Console.WriteLine("Qual time ganhou a ultima copa?");
+            string[] times = new string[4];
+            times[0] = "brasil";
+            times[1] = "franca";
+            times[2] = "mexico";
+            times[3] = "alemanha";
             
-            foreach(int variavelIntDoArray in meuArray)
-            {
-                Console.WriteLine(variavelIntDoArray); 
-
+            for(int i  = 0; i < 4; i++)
+            {                 
+                Console.WriteLine(times[i]);                                    
             }
-            */
 
-            // string txt = "Digite um numero:";
-            // int numero = 0; 
-            // Console.WriteLine(txt);     
-            // string numeroEmString = Console.ReadLine();
-            // numero = Convert.ToInt32(numeroEmString);
+            Console.WriteLine("Digite: ");
 
-            // for(int i = 0; i <= numero; i++)
-            // {
-                
-            //     Console.WriteLine(i);
-                
-            // }
+            string opcaoDoUsuario = Console.ReadLine();
+            bool ehVencedor = EhVencedor(opcaoDoUsuario); 
 
-            Console.WriteLine("Quantos numeros voce quer?");
-            string numerosEmString = Console.ReadLine();
-            int numeros = Convert.ToInt32(numerosEmString);
-
-                int resultado = 1;
-                for(int i =0; i < numeros; i++)
-                {
-                    Console.WriteLine("Escreva seu numero: ");
-                    string numeroEmString = Console.ReadLine();
-                    int numero = Convert.ToInt32(numeroEmString);
-                    resultado *= numero;
-                }
-                Console.WriteLine($"O resultado eh: {resultado}");
-
+            if(ehVencedor)
+                Console.WriteLine("venceu");
+            else
+                Console.WriteLine("perdeu");
         }
 
+        public static bool EhVencedor(string opcaoDoUsuario)
+        {
+            string vencedor = "franca";
+            return opcaoDoUsuario == vencedor;
+            
+        }
     }
-   
 }
